cd52cf4683a2498eb89d511819f1cd4a4ee6b849
kokohun <natzkornel@gmail.com>
2021-12-04 16:29:10 +0100

    Refactor Koret Enum

--- src/hazhozszallitas/Koret.java
+++ src/hazhozszallitas/Koret.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hazhozszallitas;
 
 /**
  *
- * @author Natz Kornel
+ * @author kornelnatz
+ * @author patrikgergye
  */
+
 public enum Koret {
     RIZS,
     HASABBURGONYA,
