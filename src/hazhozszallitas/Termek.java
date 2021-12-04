d5a6068fb2c239686fc3c110d6124a0a94746846
kokohun <natzkornel@gmail.com>
2021-12-04 16:57:28 +0100

    Added Termek class full structure

--- src/hazhozszallitas/Termek.java
+++ src/hazhozszallitas/Termek.java
@@ -10,9 +10,43 @@ public class Termek implements BruttoAr {
     private String nev;
     private int ar;
     private int elkeszitesiido;
+
+    public Termek(String nev, int ar, int elkeszitesiido) {
+        this.nev = nev;
+        this.ar = ar;
+        this.elkeszitesiido = elkeszitesiido;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public int getElkeszitesiido() {
+        return elkeszitesiido;
+    }
+
+    public void setElkeszitesiido(int elkeszitesiido) {
+        this.elkeszitesiido = elkeszitesiido;
+    }
     
+    public int getAr() {
+        return ar;
+    }
     
     public void setAr(int ar) {
         this.ar = (int)(ar * 1.27);
     }
+
+    @Override
+    public String toString() {
+        return "Termek Nev: " + nev + ", Ára: " + ar + ", az elkészítési ideje: " + elkeszitesiido;
+    }
+    
+    
+    
+    
 }
