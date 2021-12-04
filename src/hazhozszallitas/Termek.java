4b35d543b3d44039cefe4841cee8e8940ebc9cd2
kokohun <natzkornel@gmail.com>
2021-12-04 16:54:08 +0100

    Added BruttoAr and Tax function

--- src/hazhozszallitas/Termek.java
+++ src/hazhozszallitas/Termek.java
@@ -6,9 +6,13 @@ package hazhozszallitas;
  * @author patrikgergye
  * @author kornelnatz
  */
-public class Termek {
+public class Termek implements BruttoAr {
     private String nev;
     private int ar;
     private int elkeszitesiido;
     
+    
+    public void setAr(int ar) {
+        this.ar = (int)(ar * 1.27);
+    }
 }
